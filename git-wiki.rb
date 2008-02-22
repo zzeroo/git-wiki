3bdefe776b2cbb14bee068ce4d96d7d4f522d7cf
Jesse Andrews <anotherjesse@gmail.com>
2008-02-21 20:38:13 -0800

    move views to external erb files, and render with layout

--- git-wiki.rb
+++ git-wiki.rb
@@ -73,6 +73,13 @@ class Page
   end
 end
 
+layout { File.read('views/layout.erb') }
+
+def show(template, title)
+  @title = title
+  erb template
+end
+
 get('/') { redirect '/' + HOMEPAGE }
 get('/_stylesheet.css') { File.read(File.join(File.dirname(__FILE__), 'stylesheet.css')) }
 get('/_code.css') { File.read(File.join(File.dirname(__FILE__), 'css', "#{UV_THEME}.css")) }
@@ -83,18 +90,18 @@ get '/_list' do
   else
     @pages = $repo.commits.first.tree.contents.map { |blob| Page.new(blob.name) }
   end
-  
-  list
+
+  show(:list, 'Listing pages')  
 end
 
 get '/:page' do
   @page = Page.new(params[:page])
-  @page.tracked? ? show : redirect('/e/' + @page.name)
+  @page.tracked? ? show(:show, @page.name) : redirect('/e/' + @page.name)
 end
 
 get '/e/:page' do
   @page = Page.new(params[:page])
-  edit
+  show :edit, "Editing #{@page.name}"
 end
 
 post '/e/:page' do
@@ -105,116 +112,15 @@ end
 
 get '/h/:page' do
   @page = Page.new(params[:page])
-  history
+  show :history, "History of #{@page.name}"
 end
 
 get '/h/:page/:rev' do
   @page = Page.new(params[:page])
-  version
+  show :version, "Version of #{@page.name}"
 end
 
 get '/d/:page/:rev' do
   @page = Page.new(params[:page])
-  delta
-end
-
-def layout(title, content)
-  <<-HTML
-  <html>
-    <head>
-      <title>#{title}</title>
-      <link rel="stylesheet" href="/_stylesheet.css" type="text/css" media="screen" />
-      <link rel="stylesheet" href="/_code.css" type="text/css" media="screen" />
-      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    </head>
-    <body>
-      <div id="navigation">
-        <a href="/">Home</a>
-        <a href="/_list">List</a>
-      </div>
-      #{content}
-    </body>
-  </html>
-  HTML
-end
-
-def show
-  layout(@page.name,
-  <<-HTML
-  <a href="/e/#{@page.name}" class="edit_link">edit this page</a>
-  <a href="/h/#{@page.name}" class="edit_link">history of this page</a>
-  <h1 class="page_title">#{@page.name}</h1>
-  <div id="page_content">#{@page.body}</div>  
-  HTML
-  )
-end
-
-def version
-  layout(@page.name, 
-  <<-HTML
-  <a href="/e/#{@page.name}" class="edit_link">edit this page</a>
-  <a href="/h/#{@page.name}" class="edit_link">history of page</a>
-  <h1 class="page_title">#{@page.name}</h1>
-  <div id="page_content">#{@page.version(params[:rev])}</div>
-  HTML
-  )
-end
-
-def edit
-  layout("Editing #{@page.name}",
-  <<-HTML
-  <h1>Editing #{@page.name}</h1>
-  <a href="javascript:history.back();" class="cancel">Cancel</a>
-  <form method="post" action="/e/#{params[:page]}">
-    <p>
-      <textarea name="body" rows="25" cols="130">
-        #{@page.raw_body}
-      </textarea>
-    </p>
-    <p><input type="submit" value="Save as the newest version" class="submit" /></p>
-  </form>
-  HTML
-  )
-end
-
-def delta  
-  diff = @page.delta(params[:rev])
-  highlighted_diff = Uv.parse(diff, "xhtml", "diff", true, UV_THEME)
-  
-  layout("Diff of #{@page.name}", 
-  <<-HTML
-  <h1>Diff of <a href="/#{@page.name}">#{@page.name}</a></h1>
-  #{highlighted_diff}
-  HTML
-  )
-end
-
-def history
-  commits = ""
-  @page.history.each do |c| 
-    commits += "<li><em>#{c.committed_date}</em> "
-    commits += "<a href=\"/h/#{@page.name}/#{c.id}\">#{c.message}</a> "
-    commits += "<a href=\"/d/#{@page.name}/#{c.id}\">diff</a>" unless c.parents.empty?
-    commits += "</li>"
-  end
-  
-  layout("History of #{@page.name}",
-  <<-HTML
-  <h1>History of <a href="/#{@page.name}">#{@page.name}</a></h1>
-  <ul>#{commits}</ul>
-  HTML
-  )
-end
-
-def list
-  if @pages.empty?
-    layout('Listing pages', '<p>No pages found.</p>')
-  else
-    layout('Listing pages',   
-    <<-HTML
-    <h1>All pages</h1>
-    <ul>#{@pages.each {|page| page.to_s}}</ul>
-    HTML
-    )
-  end
+  show :delta, "Diff of #{@page.name}"
 end
